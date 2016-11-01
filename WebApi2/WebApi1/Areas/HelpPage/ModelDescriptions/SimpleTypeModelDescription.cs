24a6acbd480ff194c376033f1becc0fcaeb0bb8a
Winbringer <voin1213@yandex.ru>
2016-11-01 16:39:23 +0300

    Добавил нужные проекты в решение

---
+++ WebApi2/WebApi1/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApi1.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApi2/WebApi1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi1.WebApiApplication" Language="C#" %>
