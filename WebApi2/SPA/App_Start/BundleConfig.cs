b4b6cdf04e28e8897990888c4b8b89168b052d17
VictoremWinbringer <victortriumphator@gmail.com>
2016-11-09 16:03:38 +0300

    Добавил свои бандлы

--- WebApi2/SPA/App_Start/BundleConfig.cs
+++ WebApi2/SPA/App_Start/BundleConfig.cs
@@ -5,24 +5,15 @@ namespace SPA
 {
     public class BundleConfig
     {
-        // Дополнительные сведения об объединении см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-        //    bundles.Add(new ScriptBundle("~/bundles/js").Include(
-        //                "~/Scripts/jquery*"));
+            bundles.Add(new ScriptBundle("~/js").Include(
+                         "~/Kendo/js/jquery.min.js", "~/Kendo/js/kendo.all.min.js"));
 
-        //    // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
-        //    // используйте средство построения на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
-        //    bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-        //                "~/Scripts/modernizr-*"));
-
-        //    bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-        //              "~/Scripts/bootstrap.js",
-        //              "~/Scripts/respond.js"));
-
-        //    bundles.Add(new StyleBundle("~/Content/css").Include(
-        //              "~/Content/bootstrap.css",
-        //              "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/Kendo/styles/kendo.common.min.css",
+                      "~/Kendo/styles/kendo.bootstrap.min.css",
+                  "~/Kendo/styles/kendo.bootstrap.mobile.min.css"));
         }
     }
 }
